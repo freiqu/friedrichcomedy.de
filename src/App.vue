5f09121aeff785d1d83961f2ae1ba281b4d786e2
Friedrich Mauelshagen <f.mauelshagen@tarent.de>
2025-04-03 01:47:35 +0200

    yuh

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,7 @@
 <script setup lang="ts">
 import friedrich from './assets/friedrich.jpg'
 import FlexRow from "./components/FlexRow.vue";
-import {useI18n} from "vue-i18n";
-const { t } = useI18n()
-
-const links = [{
-  icon: 'pi pi-instagram',
-  link: 'https://www.instagram.com/friedrich.comedy',
-  name: 'Instagram'
-},{
-  icon: 'pi pi-youtube',
-  link: 'https://www.youtube.com/@friedrich.comedy',
-  name: 'Youtube'
-},{
-  icon: 'pi pi-at',
-  link: 'mailto:contact@yourfat.mom',
-  name: t('common.email')
-}]
+import links from "./content/links.ts";
 </script>
 
 <template>
@@ -37,7 +22,7 @@ const links = [{
         <template #content>
           <div class="flex flex-wrap gap-2 justify-content-center">
             <a v-for="link in links" :key="`link.${link.name}`" target="_blank" :href="link.link">
-              <Button variant="text" :icon="link.icon" v-tooltip.top="link.name"></Button>
+              <Button variant="text" :icon="link.icon" v-tooltip.top="$t(link.name)"></Button>
             </a>
           </div>
         </template>
---
+++ src/content/links.ts
@@ -0,0 +1,13 @@
+export default [{
+    icon: 'pi pi-instagram',
+    link: 'https://www.instagram.com/friedrich.comedy',
+    name: 'Instagram'
+},{
+    icon: 'pi pi-youtube',
+    link: 'https://www.youtube.com/@friedrich.comedy',
+    name: 'Youtube'
+},{
+    icon: 'pi pi-at',
+    link: 'mailto:contact@yourfat.mom',
+    name: 'common.email'
+}]
