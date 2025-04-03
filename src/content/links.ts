94da0dddd93ebc933ec6dcd241742bb8dedb1cb1
Friedrich Mauelshagen <f.mauelshagen@tarent.de>
2025-04-03 02:00:21 +0200

    add standup republic

--- src/content/links.ts
+++ src/content/links.ts
@@ -1,13 +1,22 @@
-export default [{
+export default [
+  {
     icon: 'pi pi-instagram',
     link: 'https://www.instagram.com/friedrich.comedy',
     name: 'Instagram'
-},{
+  },
+  {
     icon: 'pi pi-youtube',
     link: 'https://www.youtube.com/@friedrich.comedy',
     name: 'Youtube'
-},{
+  },
+  {
+    icon: 'pi pi-external-link',
+    link: 'https://standup-republic.de/comedians/56877400-08ec-439e-9af0-8f01766397d9',
+    name: 'Standup Republic'
+  },
+  {
     icon: 'pi pi-at',
     link: 'mailto:contact@yourfat.mom',
     name: 'common.email'
-}]
+  }
+]
