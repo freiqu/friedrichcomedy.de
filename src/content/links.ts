c0e4d0e2b5070b70a8c0c50cfe4bf2df6ea4e4bc
Friedrich Mauelshagen <f.mauelshagen@tarent.de>
2025-04-04 11:51:25 +0200

    add tiktok

--- src/content/links.ts
+++ src/content/links.ts
@@ -2,21 +2,26 @@ export default [
   {
     icon: 'pi pi-instagram',
     link: 'https://www.instagram.com/friedrich.comedy',
-    name: 'Instagram'
+    name: 'Instagram',
   },
   {
     icon: 'pi pi-youtube',
     link: 'https://www.youtube.com/@friedrich.comedy',
-    name: 'Youtube'
+    name: 'Youtube',
+  },
+  {
+    icon: 'pi pi-tiktok',
+    link: 'https://www.tiktok.com/@friedrich.comedy',
+    name: 'TikTok',
   },
   {
     icon: 'pi pi-external-link',
     link: 'https://standup-republic.de/comedians/56877400-08ec-439e-9af0-8f01766397d9',
-    name: 'Standup Republic'
+    name: 'Standup Republic',
   },
   {
     icon: 'pi pi-at',
     link: 'mailto:contact@yourfat.mom',
-    name: 'common.email'
-  }
-]
+    name: 'common.email',
+  },
+];
